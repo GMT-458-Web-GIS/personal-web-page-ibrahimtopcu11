c3e451061dbe5ed2eba2c63d2f4114583033ec44
ibrahimtopcu <ibrahimtopcu193@gmail.com>
2025-10-18 14:21:35 +0300

    Uploading env file

---
+++ env.js
@@ -0,0 +1,23 @@
+<!-- env.js -->
+<script>
+
+window.ENV = {
+  NAME: "İbrahim",
+  SURNAME: "Topcu",
+  UNIVERSITY: "Hacettepe Üniversitesi",
+  DEPARTMENT: "Geomatik Mühendisliği",
+  HOBBIES: ["Sinema izlemek", "Oyun oynamak", "Bisiklet sürmek"],
+
+
+  MAP: {
+    center: [32.8597, 39.9334], 
+    zoom: 6
+  },
+
+  SOCIAL: {
+    INSTAGRAM: ,
+    LINKEDIN:  ,
+    GITHUB:    
+  }
+};
+</script>
